d4cc8b03f5daf45ddbbcb2dd9286ab4bb19fc4f5
AmedEspinosa <amed.espinosa@mykenzie.snhu.edu>
2022-09-24 17:30:14 -0400

    pushing my current implementation of the front end

---
+++ Frontend/src/api/editEventClient.js
@@ -0,0 +1,81 @@
+import BaseClass from "../util/baseClass";
+import axios from 'axios'
+
+/**
+ * Client to call the MusicPlaylistService.
+ *
+ * This could be a great place to explore Mixins. Currently the client is being loaded multiple times on each page,
+ * which we could avoid using inheritance or Mixins.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes#Mix-ins
+ * https://javascript.info/mixins
+ */
+export default class EditEventClient extends BaseClass {
+
+    constructor(props = {}){
+        super();
+        const methodsToBind = ['clientLoaded', 'getEvent', 'editEvent'];
+        this.bindClassMethods(methodsToBind, this);
+        this.props = props;
+        this.clientLoaded(axios);
+    }
+
+    /**
+     * Run any functions that are supposed to be called once the client has loaded successfully.
+     * @param client The client that has been successfully loaded.
+     */
+    clientLoaded(client) {
+        this.client = client;
+        if (this.props.hasOwnProperty("onReady")){
+            this.props.onReady();
+        }
+    }
+
+    /**
+     * Gets the concert for the given ID.
+     * @param id Unique identifier for a concert
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     * @returns The concert
+     */
+    async getEvent(id, errorCallback) {
+        try {
+            const response = await this.client.get(`/event/${id}`);
+            return response.data;
+        } catch (error) {
+            this.handleError("getEvent", error, errorCallback)
+        }
+    }
+
+    async editEvent(
+        id, name, date, user, listOfUsersAttending, address, description, errorCallback) {
+
+        try {
+            const response = await this.client.put(`/event`,{
+                id: id,
+                name: name,
+                date: date,
+                user: user,
+                usersAttending: listOfUsersAttending,
+                address: address,
+                description: description
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError("editArtwork", error, errorCallback);
+        }
+    }
+
+    /**
+     * Helper method to log the error and run any error functions.
+     * @param error The error received from the server.
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     */
+    handleError(method, error, errorCallback) {
+        console.error(method + " failed - " + error);
+        if (error.response.data.message !== undefined) {
+            console.error(error.response.data.message);
+        }
+        if (errorCallback) {
+            errorCallback(method + " failed - " + error);
+        }
+    }
+}
