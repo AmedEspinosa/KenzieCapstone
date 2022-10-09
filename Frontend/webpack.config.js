a6a9ede7ac3315b1ef8427d51352dba2aadab6ca
AmedEspinosa <amed.espinosa@mykenzie.snhu.edu>
2022-10-09 14:44:16 -0400

    pushing final js and webpack.config

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    examplePage: path.resolve(__dirname, 'src', 'pages', 'examplePage.js'),
+    calendarPage: path.resolve(__dirname, 'src', 'pages', 'calendarPage.js'),
+    landingPage: path.resolve(__dirname, 'src', 'pages', 'landingPage.js'),
+    searchPage: path.resolve(__dirname, 'src', 'pages', 'searchPage.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -18,19 +20,64 @@ module.exports = {
     https: false,
     port: 8080,
     open: true,
-    openPage: 'http://localhost:8080',
+    openPage: 'http://localhost:8080/home_page.html',
     // diableHostChecks, otherwise we get an error about headers and the page won't render
     disableHostCheck: true,
     contentBase: 'packaging_additional_published_artifacts',
     // overlay shows a full-screen overlay in the browser when there are compiler errors or warnings
-    overlay: true
+    overlay: true,
+    proxy: [
+      {
+        context: [
+          '/event'
+        ],
+        target: 'http://localhost:5001'
+      }
+    ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/home_page.html',
+      filename: 'home_page.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/calendar.html',
+      filename: 'calendar.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/create_account_page.html',
+      filename: 'create_account_page.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/forgot_password.html',
+      filename: 'forgot_password.html',
+      inject: false
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
       inject: false
     }),
+    new HtmlWebpackPlugin({
+      template: './src/login_page.html',
+      filename: 'login_page.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/search_and_login_landing_page.html',
+      filename: 'search_and_login_landing_page.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/search_page.html',
+      filename: 'search_page.html',
+      inject: false
+    }),
+
+
     new CopyPlugin({
       patterns: [
         {
